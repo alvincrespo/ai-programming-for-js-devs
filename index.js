7379f9c5a50352bd5fe3fbd6de30b8477dd8b02e
Alvin Crespo <alvin.crespo@gmail.com>
2025-10-20 20:57:36 -0400

    feat: working with streams
    https://www.linkedin.com/learning/ai-programming-for-javascript-developers/working-with-streams

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ import OpenAI from "openai";
 const openai = new OpenAI();
 
 async function hello() {
-  const completion = await openai.chat.completions.create({
+  const stream = await openai.chat.completions.create({
     messages: [
       { role: "system", content: "You are a great robot!" },
       { role: "user", content: "What is the capital of Puerto Rico?" },
     ],
     model: "gpt-5-nano",
+    stream: true
   });
 
-  console.log(completion.choices[0]);
+  for await (const chunk of stream) {
+    process.stdout.write(chunk.choices[0].delta.content || "");
+  }
+
+  process.stdout.write("\n");
 }
 
 hello();
