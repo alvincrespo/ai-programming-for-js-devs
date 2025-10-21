6ae70b539af94b8eccb3979c1d0e33aaf36d7c13
Alvin Crespo <alvin.crespo@gmail.com>
2025-10-20 20:52:16 -0400

    feat: writing functions
    https://www.linkedin.com/learning/ai-programming-for-javascript-developers/writing-functions

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const openai = new OpenAI();
 
 async function hello() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a great robot!" }],
+    messages: [
+      { role: "system", content: "You are a great robot!" },
+      { role: "user", content: "What is the capital of Puerto Rico?" },
+    ],
     model: "gpt-5-nano",
   });
 
