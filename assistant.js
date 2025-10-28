552997c69195bb11e6ef19e750e6bd685983f9c1
Alvin Crespo <alvin.crespo@gmail.com>
2025-10-28 08:41:41 -0400

    break out assistant and conversations

--- assistant.js
+++ assistant.js
@@ -1,38 +1,8 @@
 import OpenAI from "openai";
 import "dotenv/config";
-import readline from "readline";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-});
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("What is your question for the speaker coach?  \n", async (prompt) => {
-  // Deprecated
-  // const thread = await openai.beta.threads.create()
-  // https://platform.openai.com/docs/assistants/migration
-  const conversation = await openai.conversations.create({
-    items: [
-      {
-        role: "user",
-        content: prompt
-      }
-    ]
-  });
-
-  const response = openai.responses.create({
-    conversation: conversation.id,
-    model: "gpt-4",
-    input: prompt
-  });
-
-  console.log((await response).output[0].content[0].text);
-
-  rl.close();
 });
 
 async function betterSpeaker(prompt) {
@@ -43,19 +13,6 @@ async function betterSpeaker(prompt) {
   });
 
   console.log(assistant)
-
-  // const response = await openai.assistants.chat.completions.create({
-  //   model: "gpt-4",
-  //   messages: [
-  //     {
-  //       role: "user",
-  //       content: prompt,
-  //     },
-  //   ],
-  // });
-
-  // return response;
 }
 
-
-// betterSpeaker();
+betterSpeaker();
